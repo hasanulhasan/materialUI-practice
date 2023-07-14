a31a0aa0715b00377466b2b335906efe38f69482
hasanulhasan <hasanlink71@yahoo.com>
2023-07-14 22:33:25 +0600

    search bar added

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import { MenuList } from '../data/data'
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Menu = () => {
   return (
     <Layout>
+    <Box sx={{ '& > :not(style)': { m: 2, mt: 3 } }}>
+      <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+        <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+        <TextField id="input-with-sx" label="search book" variant="standard" />
+      </Box>
+    </Box>
       <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
         {
           MenuList.map(menu => (
